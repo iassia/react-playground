1d1b7fcd0125930845008475774e004b0b117463
Fabio Iassia <fbiassia@gmail.com>
2023-07-03 22:03:48 -0300

    feat: include styled components

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,3 +1,10 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
 function Card() {
     return (
         <div className="flip-card">
@@ -5,7 +12,7 @@ function Card() {
           <div className="flip-card-front">
           </div>
           <div className="flip-card-back">
-            <h1>John Doe</h1>
+            <Title>John Doe</Title>
             <p>Architect & Engineer</p>
             <p>We love that guy</p>
           </div>
