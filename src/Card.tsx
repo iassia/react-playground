27778642cea7dcea3b1636058be0a3118e34344a
Fabio Iassia <fbiassia@gmail.com>
2023-07-04 17:59:42 -0300

    build: improve code style with prettier

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,24 +1,17 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #BF4F74;
-`;
 function Card() {
-    return (
-        <div className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
-          </div>
-          <div className="flip-card-back">
-            <Title>John Doe</Title>
-            <p>Architect & Engineer</p>
-            <p>We love that guy</p>
-          </div>
+  return (
+      <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+        </div>
+        <div className="flip-card-back">
+          <h1>John Doe</h1>
+          <p>Architect & Engineer</p>
+          <p>We love that guy</p>
         </div>
       </div>
-    )
-  }
-  
-  export default Card
+    </div>
+  )
+}
+
+export default Card
