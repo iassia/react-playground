41e9f2ea02452883a93dc35a3dc1edb234cc73a4
Fabio Iassia <fbiassia@gmail.com>
2023-07-05 21:11:44 -0300

    feat: add basic card structure

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,22 +1,53 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: #bf4f74;
+const FlipCardInner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+`
+
+const FlipCard = styled.div`
+  background-color: transparent;
+  width: 300px;
+  height: 200px;
+  perspective: 1000px;
+
+  &:hover ${FlipCardInner} {
+    transform: rotateY(180deg);
+  }
+`
+
+const FlipCardBody = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  border-radius: 10px;
+`
+
+const FlipCardBodyFront = styled(FlipCardBody)`
+  background-color: #bbb;
+  color: black;
+`
+
+const FlipCardBodyBack = styled(FlipCardBody)`
+  background-color: #2980b9;
+  color: white;
+  transform: rotateY(180deg);
 `
 
 function Card() {
   return (
-    <div className='flip-card'>
-      <div className='flip-card-inner'>
-        <div className='flip-card-front'></div>
-        <div className='flip-card-back'>
-          <Title>John Doe</Title>
-          <p>Architect & Engineer</p>
-          <p>We love that guy</p>
-        </div>
-      </div>
-    </div>
+    <FlipCard>
+      <FlipCardInner>
+        <FlipCardBodyFront></FlipCardBodyFront>
+        <FlipCardBodyBack></FlipCardBodyBack>
+      </FlipCardInner>
+    </FlipCard>
   )
 }
 
