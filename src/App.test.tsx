ee59e83e6376d95b45f5637bfb92dfaa19a456f6
Fabio Iassia <fbiassia@gmail.com>
2023-08-13 14:17:21 -0300

    test: test input data flow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,16 @@
 import App from './App'
-import { act, screen, render } from '@testing-library/react'
+import { act, screen, render, fireEvent } from '@testing-library/react'
 
 describe('App', () => {
-  it('renders the credit card', () => {
+  beforeEach(() => {
     render(<App />)
+  })
+
+  it('renders the credit card', () => {
     expect(screen.getByRole('img')).toHaveAccessibleName('Credit Card')
   })
 
   it('toggles the card face based on the cvv state', () => {
-    render(<App />)
     const cardFace = screen.getByRole('img').querySelector('div')
 
     expect(getComputedStyle(cardFace as HTMLDivElement).transform).toBe('')
@@ -20,5 +22,51 @@ describe('App', () => {
 
     act(() => screen.getByPlaceholderText('cvv').blur())
     expect(getComputedStyle(cardFace as HTMLDivElement).transform).toBe('')
+  })
+
+  describe('Input handlers', () => {
+    it('formats the number', () => {
+      const input = screen.getByPlaceholderText<HTMLInputElement>('number')
+      fireEvent.change(input, { target: { value: 'abc' } })
+      expect(input.value).toBe('')
+
+      fireEvent.change(input, { target: { value: '123' } })
+      expect(input.value).toBe('123')
+
+      fireEvent.change(input, { target: { value: '1234' } })
+      expect(input.value).toBe('1234')
+
+      fireEvent.change(input, { target: { value: '1234567890123456' } })
+      expect(input.value).toBe('1234 5678 9012 3456')
+
+      fireEvent.change(input, { target: { value: '123456789012345678' } })
+      expect(input.value).toBe('1234 5678 9012 345678')
+    })
+    it('presents', () => {
+      const input = screen.getByPlaceholderText('number')
+      fireEvent.change(input, { target: { value: '123456789012345678' } })
+      expect(screen.getByText('1234 5678 9012 345678')).toBeInTheDocument()
+    })
+
+    it('formats the valid thru date', () => {
+      const input = screen.getByPlaceholderText<HTMLInputElement>('valid-thru')
+      fireEvent.change(input, { target: { value: 'abc' } })
+      expect(input.value).toBe('')
+
+      fireEvent.change(input, { target: { value: '1020' } })
+      expect(screen.getByText('10/20')).toBeInTheDocument()
+    })
+
+    it('formats the cvv number', () => {
+      const input = screen.getByPlaceholderText('cvv')
+      fireEvent.change(input, { target: { value: ' 123 ' } })
+      expect(screen.getByText('123')).toBeInTheDocument()
+    })
+
+    it('presents the name as is', () => {
+      const input = screen.getByPlaceholderText('name')
+      fireEvent.change(input, { target: { value: 'John Doe' } })
+      expect(screen.getByText('John Doe')).toBeInTheDocument()
+    })
   })
 })
--- src/components/credit-card/FrontFace.test.tsx
+++ src/components/credit-card/FrontFace.test.tsx
@@ -1,16 +1,32 @@
 import FrontFace from './FrontFace'
 import { screen, render } from '@testing-library/react'
 
-describe('App', () => {
-  it('renders the credit card', () => {
-    const { rerender } = render(<FrontFace name='' number='' validThru='' />)
+describe('FrontFace', () => {
+  it('presents dimmed placeholder texts', () => {
+    render(<FrontFace name='' number='' validThru='' />)
 
-    let name = screen.getByText('your name here')
-    expect(getComputedStyle(name).opacity).toBe('0.5')
+    expect(
+      getComputedStyle(screen.getByText('•••• •••• •••• ••••')).opacity,
+    ).toBe('0.5')
+    expect(getComputedStyle(screen.getByText('your name here')).opacity).toBe(
+      '0.5',
+    )
+    expect(getComputedStyle(screen.getByText('••/••')).opacity).toBe('0.5')
+  })
 
-    rerender(<FrontFace name='John' number='' validThru='' />)
+  it('presents opaque texts when data is provided', () => {
+    render(
+      <FrontFace
+        name='John Doe'
+        number='1234 5678 9012 3456'
+        validThru='12/34'
+      />,
+    )
 
-    name = screen.getByText('John')
-    expect(getComputedStyle(name).opacity).toBe('1')
+    expect(
+      getComputedStyle(screen.getByText('1234 5678 9012 3456')).opacity,
+    ).toBe('1')
+    expect(getComputedStyle(screen.getByText('John Doe')).opacity).toBe('1')
+    expect(getComputedStyle(screen.getByText('12/34')).opacity).toBe('1')
   })
 })
